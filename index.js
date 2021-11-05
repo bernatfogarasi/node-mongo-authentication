33a1fe1da512b95636e25d0331fa5ec88fd3cf09
Bernat Fogarasi <fogarasi.bernat@gmail.com>
2021-11-05 23:18:31 +0000

    Request to add user to mongodb

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+mongoose.connect(
+  process.env.DATABASE_CREDENTIALS,
+  { useNewUrlParser: true },
+  (error) => console.log(error ? error : "Connected to database.")
+);
+
+app.use(express.json());
+
+app.use("/api/user", authRoute);
+
+app.listen(3000, () => console.log("Server is running..."));
+
+console.log("");
